4a8742adf4a52d278f670192ac053c61dc74f5ca
McKenzie Malo <Kenzie777kay@gmail.com>
2023-01-16 21:44:03 -0600

    first commit

---
+++ app/api/routes.py
@@ -0,0 +1,60 @@
+from lib2to3.pgen2 import token
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Book, book_schema, books_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/getdata')
+def getdata():
+    return{'yee':'haw'}
+
+@api.route('/books', methods = ['POST'])
+@token_required
+def create_book(current_user_token):
+    title = request.json['title']
+    author = request.json['author']
+    publisher = request.json['publisher']
+    language = request.json['language']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    book = Book(title, author, publisher, language, user_token=user_token )
+
+    db.session.add(book)
+    db.session.commit()
+
+    response = book_schema.dump(book)
+    return jsonify(response)
+
+@api.route('/books', methods = ['GET'])
+@token_required
+def get_book(current_user_token):
+    a_user = current_user_token.token
+    books = Book.query.filter_by(user_token = a_user).all()
+    response = books_schema.dump(books)
+    return jsonify(response)
+
+@api.route('/books/<id>', methods = ['POST','PUT'])
+@token_required
+def update_book(current_user_token,id):
+    book = Book.query.get(id) 
+    book.title = request.json['title']
+    book.author = request.json['author']
+    book.publisher = request.json['publisher']
+    book.language = request.json['language']
+    book.user_token = current_user_token.token
+
+    db.session.commit()
+    response = book_schema.dump(book)
+    return jsonify(response)
+
+@api.route('/books/<id>', methods = ['DELETE'])
+@token_required
+def delete_book(current_user_token, id):
+    book = Book.query.get(id)
+    db.session.delete(book)
+    db.session.commit()
+    response = book_schema.dump(book)
+    return jsonify(response)
